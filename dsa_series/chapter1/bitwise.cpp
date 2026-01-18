23e9732d248536610ce40e78f4a094b631e66d2a
Naib khan <=naibkhan096@gmail.com>
2026-01-18 11:47:39 -0800

    Add bitwise

---
+++ dsa_series/chapter1/bitwise.cpp
@@ -0,0 +1,118 @@
+#include <iostream> 
+using namespace std; 
+
+
+// local variable  
+
+// int fun() {
+//     int x = 10; 
+
+//     return x; 
+// }
+
+// 
+
+// global variable
+
+int x = 10; 
+
+// two times 10 10 print 
+
+// int fun() {
+//     cout<< x << endl; 
+// }
+
+int main() {
+
+    // global variable 
+
+    // fun(); 
+
+    // cout<< x <<endl; 
+
+    // 
+
+    //  x is a local variable not acces here 
+
+    //  fun(); 
+    // cout<< x << endl; 
+
+
+
+    
+
+    // && AND
+
+    // int a = 4, b = 8; 
+    // cout<<(a & b) << endl;  // output 0 
+
+
+    // 
+
+    // || OR 
+
+    // int a = 8, b = 4; 
+
+    // cout<<(a | b) << endl; // output 12  
+
+    // 
+
+    // ^ X OR
+
+    // int a = 3, b = 7; 
+    // cout<<(a ^ b)<< endl; // output 4
+
+    // 
+
+    // bitwise << 
+
+    // cout<< (4 << 1) << endl; // output 8 
+
+    // cout<< (20 << 2) << endl; 
+
+    // 
+
+    // bitwise >> 
+
+    // cout<< (10 >> 1) << endl; // output 5
+
+    // cout<< (10 >> 2) << endl; // output 2 
+
+    // 
+
+    // operator precendence 
+
+    // cout<< (5 - 2 * 6) << endl; // -7
+    // cout<< ((5 - 2) * 6) << endl; // 18    
+    // cout<< (4 * 5 % 2) << endl;  // 0 
+
+
+    // 
+
+    // local scope 
+    
+    // for(int i = 0; i < 10; i++) {
+    //     cout<<i<< endl; 
+    // }
+
+    // for(int i=0; i < 10; i++) {
+
+    // }
+    // // this is not out of block 
+    // // that's why this is not a local scope 
+    // cout<<i<< endl; 
+
+    // 
+
+    // Data types modifiers 
+
+    // cout<< sizeof(int) << endl; // 4 
+    // cout<< sizeof(long int) << endl; // 8 
+    // cout<< sizeof(short int) << endl; // 2
+    // cout<< sizeof(long long int) << endl; // 2
+
+
+
+    return 0; 
+    
+}
