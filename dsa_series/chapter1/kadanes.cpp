5a46f3be643352b1de8866842236b40c391f281d
Naib khan <=naibkhan096@gmail.com>
2026-01-29 14:11:02 -0800

    Added Kadan's Algorithms

---
+++ dsa_series/chapter1/kadanes.cpp
@@ -0,0 +1,54 @@
+#include <iostream> 
+using namespace std; 
+
+int main() {
+    
+    // basic of subarray concept 
+
+    // int n = 5;
+    // int arr[5] = {1, 2, 3, 4, 5}; 
+
+    // for(int st=0; st<n; st++) {
+    //     for(int end=st; end<n; end++) {
+    //         for(int i=st; i<=end; i++) {
+    //             cout<<arr[i]; 
+    //         }
+    //         cout<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //  
+
+    // simple for loop index value 
+
+    // int arr[5] = {1, 2, 3, 4, 5}; 
+
+    // for(int i = 0; i<5; i++) {
+    //     cout<<" index = " << i << " value = " << arr[i]<<endl;  
+    // }
+
+    // 
+
+
+    // int arr[5] = {1, 2, 3, 4, 5}; 
+    // int st = 1; 
+
+    // for(int end=st; end<5; end++) {
+    //     cout<<" st = " <<st << " end = " << end
+    //     << " value = "<< arr[end] << endl;
+    // }
+
+    // 
+
+    // maximum subarray sum 
+
+    // {3, -4, 5, 4, -1, 7, -8} 
+
+
+
+
+
+
+    return 0; 
+}
