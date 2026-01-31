7efd0595087c10d720e2295099698f327cc47af6
Naib khan <=naibkhan096@gmail.com>
2026-01-31 12:36:58 -0800

    add  Pair sum

--- dsa_series/chapter1/kadanes.cpp
+++ dsa_series/chapter1/kadanes.cpp
@@ -1,7 +1,36 @@
 #include <iostream> 
+#include <vector> 
 using namespace std; 
 
+vector<int> pairSum(vector<int> nums, int target) {
+    vector<int>ans;
+    int n = nums.size(); 
+
+    for(int i=0; i<n; i++) {
+        for(int j=i+1; j<n; j++){
+            if(nums[i] + nums[j] == target) {
+                ans.push_back(i); 
+                ans.push_back(j); 
+                return ans; 
+            }
+        }
+    }
+    return ans; 
+}
+
 int main() {
+
+     // Pair sum //////////////
+
+     vector<int> nums = {2, 7, 11, 15}; 
+     int target = 9; 
+     
+     vector<int> ans = pairSum(nums, target);
+     cout<<ans[0] << ", "<<ans[1] <<endl;  // output 0, 1
+
+
+
+    //  /////
     
     // basic of subarray concept 
 
@@ -46,9 +75,7 @@ int main() {
     // {3, -4, 5, 4, -1, 7, -8} 
 
 
+    // 
 
-
-
-
-    return 0; 
+   return 0; 
 }
